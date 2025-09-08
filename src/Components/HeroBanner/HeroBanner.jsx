c32c71202e40f283f458bbea28709f19cf228c41
Mansuri Villa <113539390+MansuriVilla@users.noreply.github.com>
2025-09-08 20:26:20 +0530

    test

--- src/Components/HeroBanner/HeroBanner.jsx
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -5,7 +5,7 @@ import HeroBannerImg from '../../assets/images/herobanner-img.png';
 export default function HeroBanner() {
   return (
     <div>
-      <h1>Welcome to my app sdfsdfsdf</h1>
+      <h1>Welcome to my app sdfsdfsdf ojiujiouj</h1>
         <img src={HeroBannerImg} alt="Hero Banner" />
     </div>
   );
