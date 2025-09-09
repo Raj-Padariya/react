1189fd9ae60beee8e7d83d946a2b7d3705c785bc
Abhishek-dev-10 <abhishek@ampvmedia.comgit config --global user.name>
2025-09-09 20:14:44 +0530

    new changed

--- src/Components/HeroBanner/HeroBanner.jsx
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -4,6 +4,10 @@ export default function HeroBanner() {
   return (
     <div>
         <img src={HeroBannerImg} alt="Hero Banner" />
+        <h1 className="text-4xl font-bold mt-4">Welcome to Our Cleaning Service</h1>
+        <p className="mt-2 text-lg text-gray-600">
+            We provide top-notch cleaning services for your home and office.
+        </p>
     </div>
   );
 }
