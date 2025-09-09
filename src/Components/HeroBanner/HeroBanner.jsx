107cbab215f14b6a0bb3f11a103c955a4d8c15a0
Abhishek-dev-10 <abhishek@ampvmedia.comgit config --global user.name>
2025-09-09 20:47:56 +0530

    changed

--- src/Components/HeroBanner/HeroBanner.jsx
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -2,8 +2,15 @@ import HeroBannerImg from '../../assets/images/herobanner-img.png';
 
 export default function HeroBanner() {
   return (
-    <div className='rounded-lg'>
-        <img src={HeroBannerImg} alt="Hero Banner" />
-    </div>
+    <section className='p-7 pb-0'>
+        <div className='relative after:absolute after:inset-0 after:bg-gradient-to-b after:from-transparent after:via-black/20 after:to-black/50 after:rounded-xl'>
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white px-4 z-10'>
+            <h1 className='text-4xl md:text-5xl lg:text-6xl font-extrabold mb-4'>Welcome to Our Service</h1>
+            <p className='text-lg md:text-xl mb-6'>Providing top-notch solutions for your needs</p>
+            <a href="#get-started" className='inline-block bg-[#E79F56] hover:bg-[#cf7c3a] text-white font-bold py-3 px-6 rounded-lg transition-colors duration-300'>Get Started</a>
+        </div>
+        <img className='rounded-xl' src={HeroBannerImg} alt="Hero Banner" />
+        </div>
+    </section>
   );
 }
