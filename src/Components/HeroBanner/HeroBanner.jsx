59c62900e22bea67c949b514b478edd3d423e80f
Abhishek <abhishek@ampvmedia.com>
2025-09-09 12:42:08 +0530

    test

--- src/Components/HeroBanner/HeroBanner.jsx
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -5,7 +5,7 @@ import HeroBannerImg from '../../assets/images/herobanner-img.png';
 export default function HeroBanner() {
   return (
     <div>
-      <h1>Welcome to my app xcvxcv</h1>
+      <h1>Welcome to my app test</h1>
         <img src={HeroBannerImg} alt="Hero Banner" />
     </div>
   );
