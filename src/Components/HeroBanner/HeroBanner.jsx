1736e37cfbf5b67595c469a467a42fa5cdfb1524
Abhishek-dev-10 <abhishek@ampvmedia.comgit config --global user.name>
2025-09-09 20:21:14 +0530

    changed

--- src/Components/HeroBanner/HeroBanner.jsx
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -2,12 +2,8 @@ import HeroBannerImg from '../../assets/images/herobanner-img.png';
 
 export default function HeroBanner() {
   return (
-    <div>
+    <div className='rounded-lg'>
         <img src={HeroBannerImg} alt="Hero Banner" />
-        <h1 className="text-4xl font-bold mt-4">Welcome to Our Cleaning Service</h1>
-        <p className="mt-2 text-lg text-gray-600">
-            We provide top-notch cleaning services for your home and office.
-        </p>
     </div>
   );
 }
